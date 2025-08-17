cc844fe60ae3907ca0994d8eae6aeedd5e1ba134
Raphael <raphael.scharff@gmail.com>
2025-08-17 18:22:06 +0200

    Theme switcher

---
+++ www/src/js/theme.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.documentElement.setAttribute("data-theme", localStorage.getItem("Theme") || "default");
+});
+
+function changeTheme() {
+    Swal.fire({
+        icon: "question",
+        title: `Change the theme`,
+        html: `
+            <form id='swal-form'>
+                <select name='theme' id='theme'>
+                    <option value='default'>Default</option>
+                    <option value='oled'>Dark OLED</option>
+                    <option value='flatui'>Flat UI</option>
+                </select>
+            </form>
+        `,
+        focusConfirm: false,
+        howCloseButton: true,
+        showCancelButton: true,
+        confirmButtonText: `Select`,
+        cancelButtonText: `Cancel`,
+    }).then((result) => {
+        if (result.value) {
+            const root = document.documentElement;
+            const theme = document.getElementById("swal-form").theme.value;
+            localStorage.setItem("Theme", theme);
+            root.setAttribute("data-theme", theme);
+        }
+    })
+}
