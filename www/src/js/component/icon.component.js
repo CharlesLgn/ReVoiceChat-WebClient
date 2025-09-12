99984eada91d6542925d34b1ddc100ed829a80f3
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2025-09-12 09:50:55 +0200

    Replace SVG with Component

--- www/src/js/component/icon.component.js
+++ www/src/js/component/icon.component.js
@@ -180,6 +180,17 @@ class CircleXIconComponent extends HTMLElement {
     }
 }
 
+class EnvelopeIconComponent extends HTMLElement {
+    constructor(){
+        super()
+        this.innerHTML = `
+            <svg data-slot="icon" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z"></path>
+                <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z"></path>
+            </svg>`
+    }
+}
+
 customElements.define('revoice-icon-pencil', PencilIconComponent);
 customElements.define('revoice-icon-trash', TrashIconComponent);
 customElements.define('revoice-icon-chat-bubble', ChatBubbleIconComponent);
@@ -196,3 +207,4 @@ customElements.define('revoice-icon-users', UsersIconComponent);
 customElements.define('revoice-icon-information', InformationIconComponent);
 customElements.define('revoice-icon-swatch', SwatchIconComponent);
 customElements.define('revoice-icon-circle-x', CircleXIconComponent);
+customElements.define('revoice-icon-envelope', EnvelopeIconComponent);
