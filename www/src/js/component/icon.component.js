c3cb5b81d7080ab55ee998ee4f5a1654d6cb40f1
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2025-09-12 09:32:03 +0200

    Replace SVG with Icon Component

--- www/src/js/component/icon.component.js
+++ www/src/js/component/icon.component.js
@@ -139,13 +139,44 @@ class Cog6ToothIconComponent extends HTMLElement {
 }
 
 class UsersIconComponent extends HTMLElement {
-    constructor(){
-        super();
+    constructor() {
+        super()
         this.innerHTML = `
             <svg data-slot="icon" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z"></path>
             </svg>
         `
+    }
+}
+
+class InformationIconComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.innerHTML = `
+            <svg data-slot="icon" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path clip-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" fill-rule="evenodd"></path>
+            </svg>`
+    }
+}
+
+class SwatchIconComponent extends HTMLElement {
+    constructor() {
+        super()
+        this.innerHTML = `
+            <svg data-slot="icon" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path clip-rule="evenodd" d="M2.25 4.125c0-1.036.84-1.875 1.875-1.875h5.25c1.036 0 1.875.84 1.875 1.875V17.25a4.5 4.5 0 1 1-9 0V4.125Zm4.5 14.25a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25Z" fill-rule="evenodd"></path>
+                <path d="M10.719 21.75h9.156c1.036 0 1.875-.84 1.875-1.875v-5.25c0-1.036-.84-1.875-1.875-1.875h-.14l-8.742 8.743c-.09.089-.18.175-.274.257ZM12.738 17.625l6.474-6.474a1.875 1.875 0 0 0 0-2.651L15.5 4.787a1.875 1.875 0 0 0-2.651 0l-.1.099V17.25c0 .126-.003.251-.01.375Z"></path>
+            </svg>`
+    }
+}
+
+class CircleXIconComponent extends HTMLElement {
+    constructor(){
+        super()
+        this.innerHTML = `
+            <svg data-slot="icon" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path clip-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" fill-rule="evenodd"></path>
+            </svg>`
     }
 }
 
@@ -162,3 +193,6 @@ customElements.define('revoice-icon-eye-open', EyeOpenIconComponent);
 customElements.define('revoice-icon-folder', FolderIconComponent);
 customElements.define('revoice-icon-cog-6', Cog6ToothIconComponent);
 customElements.define('revoice-icon-users', UsersIconComponent);
+customElements.define('revoice-icon-information', InformationIconComponent);
+customElements.define('revoice-icon-swatch', SwatchIconComponent);
+customElements.define('revoice-icon-circle-x', CircleXIconComponent);
